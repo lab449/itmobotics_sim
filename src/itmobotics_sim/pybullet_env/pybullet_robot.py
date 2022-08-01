Move urdf files in tests dirctory. Add unified reset() function. Add enumeration for robotcontrollers. Add comparison of EEState and JointState. Fix orientation calculation in reset_ee_state, and setting up number of iterations for solve inverse kinematic

src/itmobotics_sim/pybullet_env/pybullet_robot.py
@@ -1,4 +1,5 @@
 from json import tool
+from ntpath import join
 import sys, os
 import pybullet as p
 from pybullet_utils import bullet_client as bc
@@ -13,36 +14,38 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import robot
 from .urdf_editor import URDFEditor
 from spatialmath import SE3,SO3
+from spatialmath.base import r2q
 
 class PyBulletRobot(robot.Robot):
     def __init__(self, urdf_filename: str, base_transform: SE3 = SE3()):
         super().__init__(urdf_filename, base_transform)
         self.__base_urdf_filename = urdf_filename
         self.__robot_id = None
-        self.__initialize = False
+        self.__initialized = False
         self.__actuators_name_list = []
         self.__actuators_id_list = []
         self.__num_actuators = 0
+        self.__link_id = {}
 
         self.__external_models = {}
         self.__tool_list = []
 
-        self.reset()
-
     def __del__(self):
-        print(self.__external_models)
         for m in self.__external_models.keys():
             if os.path.exists(self.__external_models[m]["urdf_filename"]):
                 os.remove(self.__external_models[m]["urdf_filename"])
     
     def connect_tool(self, tool_name: str, external_urdf_filename: str, root_link: str, tf: SE3 = SE3(), save = False):
         
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
+        
         main_editor = URDFEditor(self._urdf_filename)
         child_editor = URDFEditor(external_urdf_filename)
 
         main_editor.joinURDF(child_editor, root_link, tf.A)
 
-        head = os.path.split(external_urdf_filename)[0]
+        head = '/tmp'
         newname = str(uuid.uuid4()) + '_tmp'+ '.urdf'
         newpath = os.path.join(head, newname)
         self._urdf_filename = newpath
@@ -53,11 +56,14 @@ class PyBulletRobot(robot.Robot):
         jj = robot.JointState(self.__num_actuators)
         self._update_joint_state(jj)
 
-        self.remove_robot_body()
+        
         self.reset()
         self.reset_joint_state(jj)
     
     def remove_tool(self, tool_name):
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
+
         for i in range(len(self.__tool_list)-1, -1, -1):
             last_tool = self.__tool_list[i]
             # del self.__external_models[last_tool]
@@ -72,34 +78,44 @@ class PyBulletRobot(robot.Robot):
         jj = robot.JointState(self.__num_actuators)
         self._update_joint_state(jj)
 
-        self.remove_robot_body()
         self.reset()
         self.reset_joint_state(jj)
     
     def reset_joint_state(self, jstate: robot.JointState):
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
         self._joint_state = jstate
         for i in range(self.__num_actuators):
             p.resetJointState(self.__robot_id, self.__actuators_id_list[i], self._joint_state.joint_positions[i], self._joint_state.joint_velocities[i])
     
     def reset_ee_state(self, eestate: robot.EEState):
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
         ref_frame = eestate.ref_frame
         base_ee_state = copy.deepcopy(eestate)
         if ref_frame!='world':
             refFrameState = p.getLinkState(self.__robot_id, self.__link_id[ref_frame])
             _,_,_,_, ref_frame_pos, ref_frame_rot = refFrameState
-            in_base_tf =  SE3(*ref_frame_pos)@ SO3(R.from_quat(ref_frame_rot).as_matrix(), check=False)
-            base_ee_state = in_base_tf@base_ee_state
+            in_base_tf =  SE3(*ref_frame_pos)@ SE3(SO3(R.from_quat(ref_frame_rot).as_matrix(), check=False))
+            base_ee_state.tf = in_base_tf@base_ee_state.tf
+            base_ee_state.twist = np.kron(np.eye(2,dtype=int), np.in_base_tf.R)@base_ee_state.twist
 
-        position = list(base_ee_state.tf.t)
-        orientation = p.getQuaternionFromEuler(list(base_ee_state.tf.eul()))
-        joint_poses = np.array(list(p.calculateInverseKinematics(self.__robot_id, self.__link_id[eestate.ee_link], position, orientation)) )
-        joint_speed = np.linalg.pinv(self.jacobian(joint_poses, eestate.ee_link, eestate.ref_frame)) @ base_ee_state.twist
+         # In PyBullet quaternioun described as xywz, but in spatialmath wxyz
+        position = tuple(base_ee_state.tf.t)
+        spm_quaternioun = tuple(r2q(base_ee_state.tf.R))
+        orientation = tuple([spm_quaternioun[1], spm_quaternioun[2], spm_quaternioun[0], spm_quaternioun[3]])
 
-        for i in range(self.__num_actuators):
-            p.resetJointState(self.__robot_id, self.__actuators_id_list[i], joint_poses[i], joint_speed[i])
+        js = robot.JointState(self.__num_actuators)
+        js.joint_positions = np.array(list(p.calculateInverseKinematics(self.__robot_id, self.__link_id[eestate.ee_link], position, orientation, maxNumIterations=1000, residualThreshold=1e-6)) )
+        js.joint_velocities = np.linalg.pinv(self.jacobian(js.joint_positions, eestate.ee_link, eestate.ref_frame)) @ base_ee_state.twist
+        # print(js)
+        self.reset_joint_state(js)
+        self._update_joint_state(js)
 
 
     def jacobian(self, joint_pose: np.ndarray, ee_link: str, ref_frame: str) -> np.ndarray:
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
         Jv = np.zeros((3, len(joint_pose)))
         Jw = np.zeros((3, len(joint_pose)))
 
@@ -124,24 +140,32 @@ class PyBulletRobot(robot.Robot):
         return J
     
     def apply_force_sensor(self, link: str):
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
+            
         self._force_sensor_link = link
         p.enableJointForceTorqueSensor(self.__robot_id, self.__link_id[link], 1)        
     
-    def _send_cartcontrol_position(self, position: np.ndarray):
+    def _send_cartcontrol_position(self, position: np.ndarray) -> bool:
         raise RuntimeError('Robot does not support this type of control')
 
-    def _send_cartcontrol_velocity(self, velocity: np.ndarray):
+    def _send_cartcontrol_velocity(self, velocity: np.ndarray) -> bool:
         raise RuntimeError('Robot does not support this type of control')
 
-    def _send_jointcontrol_velocity(self, velocity: np.ndarray):
+    def _send_jointcontrol_velocity(self, velocity: np.ndarray) -> bool:
+        if not self.__initialized:
+            return False
         p.setJointMotorControlArray(self.__robot_id,
             self.__actuators_id_list,
             p.VELOCITY_CONTROL,
             targetVelocities=velocity.tolist(),
             forces = self.__joint_controller_params['max_torque'].tolist()
         )
+        return True
     
-    def _send_jointcontrol_position(self, position: np.ndarray):
+    def _send_jointcontrol_position(self, position: np.ndarray) -> bool:
+        if not self.__initialized:
+            return False
         p.setJointMotorControlArray(self.__robot_id,
             self.__actuators_id_list,
             p.POSITION_CONTROL,
@@ -150,8 +174,11 @@ class PyBulletRobot(robot.Robot):
             positionGains=self.__joint_controller_params['kp'].tolist(),
             velocityGains=self.__joint_controller_params['kd'].tolist()
         )
+        return True
     
-    def _send_jointcontrol_torque(self, torque: np.ndarray):
+    def _send_jointcontrol_torque(self, torque: np.ndarray) -> bool:
+        if not self.__initialized:
+            return False
         p.setJointMotorControlArray(self.__robot_id, self.__actuators_id_list,
             p.VELOCITY_CONTROL, 
             forces=np.zeros(self.__num_actuators))
@@ -160,8 +187,11 @@ class PyBulletRobot(robot.Robot):
             controlMode = p.TORQUE_CONTROL, 
             forces = torque.tolist()
         )
+        return True
     
     def _update_cartesian_state(self, tool_state: robot.EEState):
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
         if tool_state.ee_link!='world':
             eeState = p.getLinkState(self.__robot_id, self.__link_id[tool_state.ee_link])
             _,_,_,_, link_frame_pos, link_frame_rot = eeState
@@ -176,7 +206,7 @@ class PyBulletRobot(robot.Robot):
             _,_,_,_, ref_frame_pos, ref_frame_rot = refFrameState
             tool_state.tf = (SE3(*ref_frame_pos) @ SE3(SO3(R.from_quat(ref_frame_rot).as_matrix(), check=False))).inv() @ tool_state.tf 
 
-        
+        self._update_joint_state(self.joint_state)
         tool_state.twist = self.jacobian(self.joint_state.joint_positions, tool_state.ee_link, tool_state.ref_frame).dot(
             self.joint_state.joint_velocities
         )
@@ -186,17 +216,25 @@ class PyBulletRobot(robot.Robot):
             tool_state.force_torque = np.array(pb_joint_state[0][2])
     
     def _update_joint_state(self, joint_state: robot.JointState):
+        if not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
         pb_joint_state = p.getJointStates(self.__robot_id, self.__actuators_id_list)
         joint_state.joint_positions = np.array([state[0] for state in pb_joint_state])
         joint_state.joint_velocities = np.array([state[1] for state in pb_joint_state])
         joint_state.joint_torques = np.array([state[3] for state in pb_joint_state])
     
     def remove_robot_body(self):
-        p.removeBody(self.__robot_id)
-        self.__robot_id = None
-        self.__initialize = False
+        if self.__robot_id is None:
+            return
+        elif not self.__initialized:
+            raise RuntimeError('Robot was not initialized')
+        else:
+            p.removeBody(self.__robot_id)
+            self.__robot_id = None
 
     def reset(self):
+        self.remove_robot_body()
+        self._urdf_filename = self.__base_urdf_filename
         for i in range(0, len(self.__tool_list)):
             t = self.__tool_list[i]
             if not self.__external_models[t]["save"]:
@@ -216,27 +254,28 @@ class PyBulletRobot(robot.Robot):
             self._urdf_filename,
             basePosition=self.__base_pose,
             baseOrientation=self.__base_orient,
-            useFixedBase=True
+            useFixedBase=True,
+            flags=p.URDF_USE_SELF_COLLISION
         )
         self.__num_joints = p.getNumJoints(self.__robot_id)
         self.__link_id = {}
-        if not self.__initialize:
-            self.__actuators_name_list = []
+        self.__actuators_name_list = []
+
         for _id in range(self.__num_joints):
             # print(p.getJointInfo(self.__robot_id, _id))
-            # print(p.getJointInfo(self.__robot_id, _id))
             _name = p.getJointInfo(self.__robot_id, _id)[12].decode('UTF-8')
-            if p.getJointInfo(self.__robot_id, _id)[4] != -1 and not self.__initialize:
+            if p.getJointInfo(self.__robot_id, _id)[4] != -1:
                 self.__actuators_name_list.append(_name)
             self.__link_id[_name] = _id
         
         self.__actuators_id_list = [self.__link_id[a] for a in self.__actuators_name_list]
-        # print(self.__actuators_name_list)
         self.__num_actuators = len(self.__actuators_id_list)
         self._joint_state = robot.JointState(self.__num_actuators)
         p.setJointMotorControlArray(self.__robot_id, self.__actuators_id_list,
                                     p.VELOCITY_CONTROL, 
                                     forces=np.zeros(self.__num_actuators))
+        self.__initialized = True
+
         self._send_jointcontrol_torque(np.zeros(self.__num_actuators))
         # self._force_sensor_link = None
         self.__joint_controller_params = {
@@ -245,7 +284,11 @@ class PyBulletRobot(robot.Robot):
             'max_torque': 100*np.ones(self.__num_actuators)
         }
         # print("Num joints", self.__link_id)
-        self.__initialize = True
+
+    def clear_id(self):
+       self.__initialized = False
+       self.__robot_id = None
+
 
     @property
     def joint_controller_params(self) -> dict:

src/itmobotics_sim/pybullet_env/pybullet_world.py
@@ -4,7 +4,7 @@ import pybullet_utils.bullet_client as bc
 import numpy as np
 import time
 import enum
-from spatialmath import SE3
+from spatialmath import SE3, SO3
 from scipy.spatial.transform import Rotation as R
 from .pybullet_robot import PyBulletRobot
 
@@ -20,7 +20,7 @@ class PyBulletWorld():
         self.__time_scale = max(time_scale, 1.0)
         assert self.__time_scale < 1e3, "Large time scale doesn't support, please chose less than 1e3"
         assert self.__time_step < 1.0, "Large time step doesn't support, please chose less than 1.0 sec"
-        self.__robots = []
+        self.__robots = {}
 
         self.__pybullet_gui_mode = p.DIRECT
         
@@ -33,8 +33,12 @@ class PyBulletWorld():
         self.__objects = {}
         self.reset()
     
-    def add_robot(self, robot: PyBulletRobot):
-        self.__robots.append(robot)
+    def add_robot(self, robot: PyBulletRobot, name: str = 'robot') -> bool:
+        if name in self.__robots.keys():
+            return False
+        self.__robots[name] = robot
+        robot.reset()
+        return True
     
     def add_object(self, name:str, urdf_filename: str, base_transform: SE3 = SE3(), fixed=True, save=False):
         if name in self.__objects.keys():
@@ -78,8 +82,9 @@ class PyBulletWorld():
     
     
     def reset(self):
-        for r in self.__robots:
-            r.remove_robot_body()
+        for r in self.__robots.keys():
+            self.__robots[r].clear_id()
+
         p.resetSimulation()
         p.setGravity(0, 0, -9.82)
         p.setTimeStep(self.__time_step)
@@ -87,6 +92,10 @@ class PyBulletWorld():
         p.setRealTimeSimulation(False)
 
         self.__world_model = p.loadURDF(self.__urdf_filename, useFixedBase=True)
+        
+        for r in self.__robots.keys():
+            self.__robots[r].reset()
+
         self.__sim_time = 0.0
         self.__start_sim_time = time.time()
         for n in self.__objects:
@@ -94,7 +103,6 @@ class PyBulletWorld():
             if obj["save"]:
                 self.__append_object(n, obj["urdf_filename"], obj["base_tf"], obj["fixed"], obj["save"])
         
-        print(self.__objects)
 
     @property
     def sim_time(self) -> float:

src/itmobotics_sim/utils/controllers.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import numpy as np
 import scipy
 from abc import ABC, abstractmethod
-from .robot import EEState, JointState, Robot, Motion
+from .robot import RobotControllerType, EEState, JointState, Robot, Motion
 from spatialmath import SE3, SO3
 
 import copy
@@ -78,13 +78,13 @@ class MPIDController(VectorController):
         assert D.shape[0] == self.__D.shape[0] and D.shape[1] == self.__D.shape[1],'Invalid input matrix size, expected {:d}x{:d}, but given {:d}x{:d}'.format(self.__D.shape[0], self.__D.shape[1], D.shape[0], D.shape[1])
         self.__D = D        
 
-class RobotController(ABC):
+class ExternalController(ABC):
     def __init__(self, rob: Robot, robot_controller_type: str):
         self.robot = rob
         self.__robot_controller_type = robot_controller_type
         self.__child_controller = None
 
-    def connect_controller(self, controller: RobotController):
+    def connect_controller(self, controller: ExternalController):
         self.__child_controller = controller
 
     @abstractmethod
@@ -100,10 +100,17 @@ class RobotController(ABC):
             return self.__child_controller.send_control_to_robot(target_motion)
         return self.robot.set_control(target_motion, self.__robot_controller_type)
 
+class SimpleController(ExternalController):
+    def __init__(self, rob: Robot, robot_controller_type: str):
+        super().__init__(rob, robot_controller_type)
+    
+    def calc_control(self, target_motion: Motion) -> bool:
+        return True
 
-class CartVelocityToJointVelocityController(RobotController):
+
+class CartVelocityToJointVelocityController(ExternalController):
     def __init__(self, robot: Robot):
-        super().__init__(robot, 'joint_velocities')
+        super().__init__(robot, RobotControllerType.JOINT_VELOCITIES)
     
     def calc_control(self, target_motion: Motion)-> bool:
         target_motion.joint_state.joint_velocities =  np.linalg.pinv(
@@ -113,17 +120,21 @@ class CartVelocityToJointVelocityController(RobotController):
         ) @ target_motion.ee_state.twist
         return True
 
-class JointTorquesController(RobotController):
+class JointTorquesController(SimpleController):
     def __init__(self, robot: Robot):
-        super().__init__(robot, 'joint_torques')
-    
-    def calc_control(self, target_motion: Motion)-> bool:
-        return True
+        super().__init__(robot, RobotControllerType.JOINT_TORQUES)
 
+class JointPositionsController(SimpleController):
+    def __init__(self, robot: Robot):
+        super().__init__(robot, RobotControllerType.JOINT_POSITIONS)
 
-class CartPositionToCartVelocityController(RobotController):
+class JointVelocitiesController(SimpleController):
+    def __init__(self, robot: Robot):
+        super().__init__(robot, RobotControllerType.JOINT_VELOCITIES)
+
+class CartPositionToCartVelocityController(ExternalController):
     def __init__(self, robot):
-        super().__init__(robot, 'twist')       
+        super().__init__(robot, RobotControllerType.TWIST)       
         self.__pid =  MPIDController(10*np.identity(6), 1e-4*np.identity(6), 1e-1*np.identity(6), 1e-3)
 
     def calc_control(self, target_motion: Motion)-> bool:
@@ -142,9 +153,9 @@ class CartPositionToCartVelocityController(RobotController):
         target_motion.ee_state.twist = target_twist
         return True
 
-class CartForceHybrideToCartVelocityController(RobotController):
+class CartForceHybrideToCartVelocityController(ExternalController):
     def __init__(self, robot: Robot, selected_axis: np.ndarray, stiffnes: np.ndarray, ref_basis: str = 'world'):
-        super().__init__(robot, 'twist')
+        super().__init__(robot, RobotControllerType.TWIST)
         self.__pid =  MPIDController(10*np.identity(6), 1e-4*np.identity(6), 1e-1*np.identity(6), 1e-3)
         self.__ref_basis = ref_basis
         self.__stiffnes = stiffnes

src/itmobotics_sim/utils/robot.py
@@ -1,17 +1,23 @@
 from __future__ import annotations
+from enum import Enum, auto
 
 import numpy as np
 from abc import ABC, abstractmethod
 from spatialmath import SE3, SO3
 
-
+class RobotControllerType(Enum):
+    JOINT_POSITIONS = 'joint_positions'
+    JOINT_VELOCITIES = 'joint_velocities'
+    JOINT_TORQUES = 'joint_torques'
+    TF = 'tf'
+    TWIST = 'twist'
 
 class JointState():
     def __init__(self, num_joints: int):
         self.__num_joints = num_joints
         self.__joint_positions: np.ndarray = None
         self.__joint_velocities: np.ndarray = None
-        self.__joint_torques: np.ndarray =  None
+        self.__joint_torques: np.ndarray =  np.zeros(num_joints)
 
     def __str__(self):
         out = 'Position: {:s},\nVelocity: {:s},\nTorque: {:s}'.format(
@@ -20,6 +26,9 @@ class JointState():
             str(np.round(self.__joint_torques.tolist(),decimals=4)),
             )
         return out
+
+    def __eq__(self, __js: JointState) -> bool:
+        return np.equal(__js.joint_positions, self.joint_positions).all() and np.equal(__js.joint_velocities, self.joint_velocities).all() and np.equal(__js.joint_torques, self.joint_torques).all()
     
     def from_position(jpose: np.ndarray) -> JointState:
         js = JointState(jpose.shape[0])
@@ -87,7 +96,10 @@ class EEState():
             str(np.round(self.__force_torque.tolist(),decimals=3)),
             )
         return out
-    
+
+    def __eq__(self, __es: EEState) -> bool:
+        return np.allclose(self.__tf.A, __es.tf.A, atol=1e-5) and np.allclose(self.__twist, __es.twist, atol=1e-5) 
+
     def copy(self) -> EEState:
         return 
 
@@ -188,31 +200,31 @@ class Robot(ABC):
         self._joint_state = JointState(6)
         self._force_sensor_link = None
         self.__controllers_types ={
-            'joint_positions': self._send_jointcontrol_position,
-            'joint_velocities': self._send_jointcontrol_velocity,
-            'joint_torques': self._send_jointcontrol_torque,
-            'tf': self._send_cartcontrol_position,
-            'twist': self._send_cartcontrol_velocity
+            RobotControllerType.JOINT_POSITIONS.value: self._send_jointcontrol_position,
+            RobotControllerType.JOINT_VELOCITIES.value: self._send_jointcontrol_velocity,
+            RobotControllerType.JOINT_TORQUES.value: self._send_jointcontrol_torque,
+            RobotControllerType.TF.value: self._send_cartcontrol_position,
+            RobotControllerType.TWIST.value: self._send_cartcontrol_velocity
         }
 
     @abstractmethod
-    def _send_jointcontrol_position(self, position: np.ndarray):
+    def _send_jointcontrol_position(self, position: np.ndarray) -> bool:
         pass
 
     @abstractmethod
-    def _send_jointcontrol_velocity(self, velocity: np.ndarray):
+    def _send_jointcontrol_velocity(self, velocity: np.ndarray) -> bool:
         pass
 
     @abstractmethod
-    def _send_cartcontrol_position(self, tf: SE3()):
+    def _send_cartcontrol_position(self, tf: SE3()) -> bool:
         pass
 
     @abstractmethod
-    def _send_cartcontrol_velocity(self, velocity: np.ndarray):
+    def _send_cartcontrol_velocity(self, velocity: np.ndarray) -> bool:
         pass
 
     @abstractmethod
-    def _send_jointcontrol_torque(self, torque: np.ndarray):
+    def _send_jointcontrol_torque(self, torque: np.ndarray) -> bool:
         pass
 
     @abstractmethod
@@ -244,24 +256,27 @@ class Robot(ABC):
         self._update_joint_state(self._joint_state)
         return self._joint_state
     
+    @property
+    def num_joints(self) -> JointState:
+        return self._joint_state.num_joints
+
     def ee_state(self, ee_link: str, ref_frame: str = 'world') -> EEState:
         tool_state = EEState(ee_link, ref_frame)
         self._update_joint_state(self._joint_state)
         self._update_cartesian_state(tool_state)
         return tool_state
 
-    def set_control(self, target_motion: Motion, type_ctrl: str) -> bool:
-        assert type_ctrl in self.__controllers_types, "Unknown joint controller type: {:s} ".format(type_ctrl)
+    def set_control(self, target_motion: Motion, type_ctrl: RobotControllerType) -> bool:
+        assert type_ctrl.value in self.__controllers_types, "Unknown joint controller type: {:s} ".format(type_ctrl)
         target = None
         try:
-            target = getattr(target_motion.joint_state, type_ctrl)
+            target = getattr(target_motion.joint_state, type_ctrl.value)
         except AttributeError:
             try:
-                target = getattr(target_motion.ee_state, type_ctrl)
+                target = getattr(target_motion.ee_state, type_ctrl.value)
             except AttributeError:
                 pass
         if not target is None:
-            self.__controllers_types[type_ctrl](target)
-            return True
+            return self.__controllers_types[type_ctrl.value](target)
         return False
 