84fddffaf16027f15b312c4341c1f85bf0d4c629
TexnoMann <ElectroDans@yandex.ru>
2022-08-01 22:05:50 +0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='itmobotics_sim',
-    version='0.0.2',
+    version='0.0.3',
     author='TexnoMann',
     author_email='texnoman@itmo.ru',
     description='Package with pybullet robots simulation',
