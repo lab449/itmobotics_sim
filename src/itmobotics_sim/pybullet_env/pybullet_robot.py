Fix twist calculation if it desired in some reference frame

src/itmobotics_sim/pybullet_env/pybullet_robot.py
@@ -30,6 +30,8 @@ class PyBulletRobot(robot.Robot):
         self.__external_models = {}
         self.__tool_list = []
 
+        self.__joint_limits: robot.JointLimits = None
+
     def __del__(self):
         for m in self.__external_models.keys():
             if os.path.exists(self.__external_models[m]["urdf_filename"]):
@@ -111,8 +113,8 @@ class PyBulletRobot(robot.Robot):
             refFrameState = p.getLinkState(self.__robot_id, self.__joint_id_for_link[ref_frame])
             _,_,_,_, ref_frame_pos, ref_frame_rot = refFrameState
             in_base_tf =  SE3(*ref_frame_pos)@ SE3(SO3(R.from_quat(ref_frame_rot).as_matrix(), check=False))
-            base_ee_state.tf = in_base_tf@base_ee_state.tf
-            base_ee_state.twist = np.kron(np.eye(2,dtype=int), in_base_tf.R)@base_ee_state.twist
+            base_ee_state.tf = in_base_tf @ base_ee_state.tf
+            base_ee_state.twist = np.kron(np.eye(2,dtype=int), in_base_tf.R) @ base_ee_state.twist
 
          # In PyBullet quaternioun described as xywz, but in spatialmath wxyz
         position = tuple(base_ee_state.tf.t)
@@ -152,10 +154,10 @@ class PyBulletRobot(robot.Robot):
         J = np.concatenate((Jv,Jw), axis=0)
         return J
     
-    def _send_cartcontrol_position(self, position: np.ndarray) -> bool:
+    def _send_eecontrol_position(self, position: np.ndarray) -> bool:
         raise RuntimeError('Robot does not support this type of control')
 
-    def _send_cartcontrol_velocity(self, velocity: np.ndarray) -> bool:
+    def _send_eecontrol_velocity(self, velocity: np.ndarray) -> bool:
         raise RuntimeError('Robot does not support this type of control')
 
     def _send_jointcontrol_velocity(self, velocity: np.ndarray) -> bool:
@@ -195,32 +197,32 @@ class PyBulletRobot(robot.Robot):
         )
         return True
     
-    def _update_cartesian_state(self, tool_state: robot.EEState):
+    def _update_ee_state(self, tool_state: robot.EEState):
         # print(p.getNumJoints(self.__robot_id))
         if not self.__initialized:
             raise RuntimeError('Robot was not initialized')
         if tool_state.ee_link!='world':
-            eeState = p.getLinkState(self.__robot_id, self.__joint_id_for_link[tool_state.ee_link])
-            _,_,_,_, link_frame_pos, link_frame_rot = eeState
+            eeState = p.getLinkState(self.__robot_id, self.__joint_id_for_link[tool_state.ee_link], computeLinkVelocity=1)
+            _,_,_,_, link_frame_pos, link_frame_rot, link_frame_pos_vel, link_frame_rot_vel = eeState
         else:
             link_frame_pos = np.zeros(3)
             link_frame_rot = np.array([0,0,0,1])
         
         tool_state.tf = SE3(*link_frame_pos) @ SE3(SO3(R.from_quat(link_frame_rot).as_matrix(), check=False))
-
-        self._update_joint_state(self.joint_state)
-        tool_state.twist = self.jacobian(self.joint_state.joint_positions, tool_state.ee_link, tool_state.ref_frame).dot(
-            self.joint_state.joint_velocities
-        )
+        tool_state.twist = np.concatenate([link_frame_pos_vel, link_frame_rot_vel])
         
         pb_joint_state = p.getJointStates(self.__robot_id, [self.__joint_id_for_link[tool_state.ee_link]])
         tool_state.force_torque = np.array(pb_joint_state[0][2])
 
         if tool_state.ref_frame != 'world':
-            refFrameState = p.getLinkState(self.__robot_id, self.__joint_id_for_link[tool_state.ref_frame])
-            _,_,_,_, ref_frame_pos, ref_frame_rot = refFrameState
+            refFrameState = p.getLinkState(self.__robot_id, self.__joint_id_for_link[tool_state.ref_frame], computeLinkVelocity=1)
+            _,_,_,_, ref_frame_pos, ref_frame_rot, ref_frame_pos_vel, ref_frame_rot_vel  = refFrameState
+            ref_frame_twist = np.concatenate([ref_frame_pos_vel, ref_frame_rot_vel])
+
             tool_state.tf = (SE3(*ref_frame_pos) @ SE3(SO3(R.from_quat(ref_frame_rot).as_matrix(), check=False))).inv() @ tool_state.tf
-            tool_state.force_torque = np.kron(np.eye(2,dtype=int), R.from_quat(ref_frame_rot).as_matrix()) @ tool_state.force_torque
+            rotation_6d = np.kron(np.eye(2,dtype=int), R.from_quat(ref_frame_rot).inv().as_matrix())
+            tool_state.twist = rotation_6d @ (tool_state.twist - ref_frame_twist)
+            tool_state.force_torque = rotation_6d @ tool_state.force_torque
     
     def _update_joint_state(self, joint_state: robot.JointState):
         if not self.__initialized:
@@ -229,9 +231,6 @@ class PyBulletRobot(robot.Robot):
         joint_state.joint_positions = np.array([state[0] for state in pb_joint_state])
         joint_state.joint_velocities = np.array([state[1] for state in pb_joint_state])
         joint_state.joint_torques = np.array([state[3] for state in pb_joint_state])
-    
-    def apply_force_sensor(self, link: str):
-        pass
     
     def __remove_robot_body(self):
         if self.__robot_id is None:
@@ -259,15 +258,33 @@ class PyBulletRobot(robot.Robot):
         self.__joint_id_for_link = {}
         self.__actuators_name_list = []
 
+        _p_limits = [[], []]
+        _v_limits = [[], []]
+        _t_limits = [[], []]
         for _id in range(p.getNumJoints(self.__robot_id)):
             # print(p.getJointInfo(self.__robot_id, _id))
-            _name = p.getJointInfo(self.__robot_id, _id)[12].decode('UTF-8')
-            if p.getJointInfo(self.__robot_id, _id)[4] != -1:
+            joint_info = p.getJointInfo(self.__robot_id, _id)
+
+            _name = joint_info[12].decode('UTF-8')
+            if joint_info[4] != -1:
                 self.__actuators_name_list.append(_name)
+                _p_limits[0].append(joint_info[8]); _p_limits[1].append(joint_info[9])
+                _t_limits[0].append(-joint_info[10]); _t_limits[1].append(joint_info[10])
+                _v_limits[0].append(-joint_info[11]); _v_limits[1].append(joint_info[11])
             self.__joint_id_for_link[_name] = _id
         
         self.__actuators_id_list = [self.__joint_id_for_link[a] for a in self.__actuators_name_list]
         self.__num_actuators = len(self.__actuators_id_list)
+        for i in range(0, 2):
+            _p_limits[i] = np.array(_p_limits[i])
+            _v_limits[i] = np.array(_v_limits[i])
+            _t_limits[i] = np.array(_t_limits[i])
+        # print(tuple(_p_limits))
+        self.__joint_limits = robot.JointLimits(
+            tuple(_p_limits),
+            tuple(_v_limits),
+            tuple(_t_limits)
+        )
         self._joint_state = robot.JointState(self.__num_actuators)
         p.setJointMotorControlArray(self.__robot_id, self.__actuators_id_list,
                                     p.VELOCITY_CONTROL, 
@@ -285,6 +302,9 @@ class PyBulletRobot(robot.Robot):
 
         for _id in range(p.getNumJoints(self.__robot_id)):
             p.enableJointForceTorqueSensor(self.__robot_id, _id, 1)
+        
+        self._update_joint_state(self._joint_state)
+        
 
     def clear_id(self):
        self.__initialized = False
@@ -298,6 +318,10 @@ class PyBulletRobot(robot.Robot):
     @property
     def robot_id(self) -> int:
         return self.__robot_id
+    
+    @property
+    def joint_limits(self) -> robot.JointLimits:
+        return self.__joint_limits
     
     def link_id(self, link_name: str) -> int:
         return self.__joint_id_for_link[link_name]

src/itmobotics_sim/pybullet_env/pybullet_world.py
@@ -1,3 +1,5 @@
+import os, sys
+
 import pybullet as p
 import pybullet_data
 import pybullet_utils.bullet_client as bc
@@ -7,6 +9,9 @@ import enum
 from spatialmath import SE3, SO3
 from scipy.spatial.transform import Rotation as R
 from .pybullet_robot import PyBulletRobot
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils import robot
 
 class GUI_MODE(enum.Enum):
     DIRECT = enum.auto()
@@ -32,6 +37,10 @@ class PyBulletWorld():
 
         self.__objects = {}
         self.reset()
+    
+    def __del__(self):
+        print("Pybullet disconnecting")
+        p.disconnect()
     
     def add_robot(self, robot: PyBulletRobot, name: str = 'robot') -> bool:
         if name in self.__robots.keys():
@@ -71,21 +80,24 @@ class PyBulletWorld():
         p.removeBody(self.__objects[name]["id"])
         del self.__objects[name]
 
-    def link_tf(self, model_name: str, link: str, reference_model_name: str, reference_link: str) -> SE3:
-        if model_name in self.__objects:
-            pr = p.getLinkState(self.__objects[model_name]["id"], self.__objects[model_name]["link_id"][link])
-        elif model_name in self.__robots:
-            pr = p.getLinkState(self.__robots[model_name].robot_id, self.__robots[model_name].link_id(link))
-        else:
-            raise RuntimeError('Unknown model name. Please check that object or robot model has been added to the simulator with name: {:s}.\n List of added robot models: {:s}.\n List of added object models: {:s}'.format(model_name, str(list(self.__robots.keys())), str(list(self.__objects.keys()))))
-        _,_,_,_, link_frame_pos, link_frame_rot = pr
-        tf_link_in_to_world = SE3(*link_frame_pos) @ SE3(SO3(R.from_quat(link_frame_rot).as_matrix(), check=False))
-        print(tf_link_in_to_world)
-        if reference_model_name == "" or reference_link == "world":
-            return tf_link_in_to_world
+    def link_state(self, model_name: str, link: str, reference_model_name: str, reference_link: str) -> robot.EEState:
+        link_state = robot.EEState.from_tf(SE3(0.0, 0.0, 0.0), ee_link=link, ref_link=reference_link)
+        if link != 'world':
+            if model_name in self.__objects:
+                pr = p.getLinkState(self.__objects[model_name]["id"], self.__objects[model_name]["link_id"][link], computeLinkVelocity=1)
+            elif model_name in self.__robots:
+                pr = p.getLinkState(self.__robots[model_name].robot_id, self.__robots[model_name].link_id(link), computeLinkVelocity=1)
+            else:
+                raise RuntimeError('Unknown model name. Please check that object or robot model has been added to the simulator with name: {:s}.\n List of added robot models: {:s}.\n List of added object models: {:s}'.format(model_name, str(list(self.__robots.keys())), str(list(self.__objects.keys()))))
+            
+            _,_,_,_, link_frame_pos, link_frame_rot, link_frame_pos_vel, link_frame_rot_vel = pr
+            link_state.tf = SE3(*link_frame_pos) @ SE3(SO3(R.from_quat(link_frame_rot).as_matrix(), check=False))
+            
 
-        # print(self.__robots)
-        # print(reference_model_name)
+        if reference_model_name == "" or reference_link == "world":
+            return link_state
+
+
         if reference_model_name in self.__objects:
             pr = p.getLinkState(self.__objects[reference_model_name]["id"], self.__objects[reference_model_name]["link_id"][reference_link])
         elif reference_model_name in self.__robots:
@@ -94,10 +106,8 @@ class PyBulletWorld():
             raise RuntimeError('Unknown reference model name. Please check that object or robot model has been added to the simulator with name: {:s}.\n List of added robot models: {:s}.\n List of added object models: {:s}'.format(reference_model_name, str(list(self.__robots.keys())), str(list(self.__objects.keys()))))
         _,_,_,_, link_frame_pos, link_frame_rot = pr
         reference_tf_link_in_to_world = SE3(*link_frame_pos) @ SE3(SO3(R.from_quat(link_frame_rot).as_matrix(), check=False))
-        print(reference_tf_link_in_to_world)
 
         tf_in_to_reference = reference_tf_link_in_to_world.inv() @ tf_link_in_to_world
-        print(tf_in_to_reference)
         return tf_in_to_reference
     
 

src/itmobotics_sim/utils/controllers.py
@@ -108,7 +108,7 @@ class SimpleController(ExternalController):
         return True
 
 
-class CartVelocityToJointVelocityController(ExternalController):
+class EEVelocityToJointVelocityController(ExternalController):
     def __init__(self, robot: Robot):
         super().__init__(robot, RobotControllerType.JOINT_VELOCITIES)
     
@@ -132,7 +132,7 @@ class JointVelocitiesController(SimpleController):
     def __init__(self, robot: Robot):
         super().__init__(robot, RobotControllerType.JOINT_VELOCITIES)
 
-class CartPositionToCartVelocityController(ExternalController):
+class EEPositionToEEVelocityController(ExternalController):
     def __init__(self, robot):
         super().__init__(robot, RobotControllerType.TWIST)       
         self.__pid =  MPIDController(10*np.identity(6), 1e-4*np.identity(6), 1e-1*np.identity(6), 1e-3)
@@ -153,13 +153,13 @@ class CartPositionToCartVelocityController(ExternalController):
         target_motion.ee_state.twist = target_twist
         return True
 
-class CartForceHybrideToCartVelocityController(ExternalController):
+class EEForceHybrideToEEVelocityController(ExternalController):
     def __init__(self, robot: Robot, selected_axis: np.ndarray, stiffnes: np.ndarray, ref_basis: str = 'world'):
         super().__init__(robot, RobotControllerType.TWIST)
         self.__pid =  MPIDController(10*np.identity(6), 1e-4*np.identity(6), 1e-1*np.identity(6), 1e-3)
         self.__ref_basis = ref_basis
         self.__stiffnes = stiffnes
-        self.__T, self.__Y = CartForceHybrideToCartVelocityController.generate_square_selection_matrix(selected_axis)
+        self.__T, self.__Y = EEForceHybrideToEEVelocityController.generate_square_selection_matrix(selected_axis)
 
     def calc_control(self, target_motion: Motion)-> bool:
         assert isinstance(target_motion, Motion), "Invalid type of target state, expected {:s}, but given {:s}". format(str(Motion), str(type(target_motion)))

src/itmobotics_sim/utils/robot.py
@@ -1,15 +1,103 @@
 from __future__ import annotations
+import functools
+import operator
+
 from enum import Enum, auto
+from typing import Tuple
 
 import numpy as np
 from abc import ABC, abstractmethod
 from spatialmath import SE3
+
 class RobotControllerType(Enum):
     JOINT_POSITIONS = 'joint_positions'
     JOINT_VELOCITIES = 'joint_velocities'
     JOINT_TORQUES = 'joint_torques'
     TF = 'tf'
     TWIST = 'twist'
+
+class JointLimits():
+    def __init__(self, limit_positions: Tuple[np.ndarray],  limit_velocities: Tuple[np.ndarray], limit_torques: Tuple[np.ndarray]):
+        assert limit_positions[0].shape[0]==limit_positions[1].shape[0], \
+            "Diffrent shapes of lower an upper limits: {:d} and {:d} respectively".format(
+            limit_positions[0].shape[0], limit_positions[1].shape[0]
+        )
+        self.__num_joints = limit_positions[0].shape[0]
+        self.limit_positions = limit_positions
+
+        if limit_velocities is None:
+            self.limit_velocities = self.__limit_positions
+        else:
+            self.limit_velocities = limit_velocities
+
+        if limit_torques is None:
+            self.limit_torques = (1000*np.zeros(self.__num_joints), 1000*np.zeros(self.__num_joints))
+        else:
+            self.limit_torques = limit_torques
+
+    def __str__(self):
+        output_list = functools.reduce(
+            operator.iconcat, [
+                self.__limit_positions,
+                self.__limit_velocities,
+                self.__limit_torques
+            ], []
+        )
+        out = ('Joint positions limits:\n   min: {:s}, max: {:s},\n'+
+                'Joint velocities limits:\n   min: {:s}, max: {:s},\n'+
+                'Joint torques limits: \n   min: {:s}, max: {:s}').format(
+                *[str(v) for v in output_list]
+                )
+        return out
+    
+    def clip_joint_state(self, joint_state: JointState):
+        np.clip(joint_state.joint_positions, self.__limit_positions[0], self.__limit_positions[1])
+        np.clip(joint_state.joint_velocities, self.__limit_velocities[0], self.__limit_velocities[1])
+        np.clip(joint_state.joint_torques, self.__limit_torques[0], self.__limit_torques[1])
+    
+    @property
+    def num_joints(self) -> int:
+        return self.__num_joints
+
+    @property
+    def limit_positions(self) -> np.ndarray:
+        return self.__limit_positions
+    
+    @property
+    def limit_velocities(self) -> np.ndarray:
+        return self.__limit_velocities
+    
+    @property
+    def limit_torques(self) -> np.ndarray:
+        return self.__limit_torques
+    
+    
+    @limit_positions.setter
+    def limit_positions(self, limit: Tuple[np.ndarray]):
+        assert limit[0].shape[0]==limit[1].shape[0], \
+            "Diffrent shapes of lower an upper limits: {:d} and {:d} respectively".format(
+            limit[0].shape[0], limit[1].shape[0]
+        )
+        assert self.__num_joints == limit[0].shape[0],'Invalid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jpose.shape[0])
+        self.__limit_positions = limit
+
+    @limit_velocities.setter
+    def limit_velocities(self,  limit: Tuple[np.ndarray]):
+        assert limit[0].shape[0]==limit[1].shape[0], \
+            "Diffrent shapes of lower an upper limits: {:d} and {:d} respectively".format(
+            limit[0].shape[0], limit[1].shape[0]
+        )
+        assert self.__num_joints == limit[0].shape[0],'Invalid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jpose.shape[0])
+        self.__limit_velocities = limit
+    
+    @limit_torques.setter
+    def limit_torques(self,  limit: Tuple[np.ndarray]):
+        assert limit[0].shape[0]==limit[1].shape[0], \
+            "Diffrent shapes of lower an upper limits: {:d} and {:d} respectively".format(
+            limit[0].shape[0], limit[1].shape[0]
+        )
+        assert self.__num_joints == limit[0].shape[0],'Invalid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jpose.shape[0])
+        self.__limit_torques = limit
 
 class JointState():
     def __init__(self, num_joints: int):
@@ -64,19 +152,18 @@ class JointState():
     
     @joint_positions.setter
     def joint_positions(self, jpose: np.ndarray):
-        assert self.__num_joints == jpose.shape[0],'Invaliid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jpose.shape[0])
+        assert self.__num_joints == jpose.shape[0],'Invalid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jpose.shape[0])
         self.__joint_positions = jpose
 
     @joint_velocities.setter
     def joint_velocities(self, jvel: np.ndarray):
-        assert self.__num_joints == jvel.shape[0],'Invaliid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jvel.shape[0])
+        assert self.__num_joints == jvel.shape[0],'Invalid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jvel.shape[0])
         self.__joint_velocities = jvel
     
     @joint_torques.setter
     def joint_torques(self, jtorq: np.ndarray):
-        assert self.__num_joints == jtorq.shape[0],'Invaliid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jtorq.shape[0])
+        assert self.__num_joints == jtorq.shape[0],'Invalid input vector size, expected {:d}, but given {:d}'.format(self.__num_joints, jtorq.shape[0])
         self.__joint_torques = jtorq
-
 
 class EEState():
     def __init__(self, ee_link: str, ref_frame: str = 'world'):
@@ -102,20 +189,26 @@ class EEState():
     def copy(self) -> EEState:
         return 
 
-    def from_force_torque(force_torque: np.ndarray, ee_link:str) -> EEState:
-        es = EEState(ee_link)
+    def from_force_torque(force_torque: np.ndarray, ee_link:str, ref_link: str = 'world') -> EEState:
+        es = EEState(ee_link, ref_link)
         es.force_torque = force_torque
         return es
     
-    def from_twist(twist: np.ndarray, ee_link:str) -> EEState:
-        es = EEState(ee_link)
+    def from_twist(twist: np.ndarray, ee_link:str, ref_link: str = 'world') -> EEState:
+        es = EEState(ee_link, ref_link)
         es.twist = twist
         return es
     
-    def from_tf(tf: SE3, ee_link:str)-> EEState:
-        es = EEState(ee_link)
+    def from_tf(tf: SE3, ee_link:str, ref_link: str = 'world')-> EEState:
+        es = EEState(ee_link, ref_link)
         es.tf = tf
         return es
+    
+    def transform(self, tf: SE3):
+        self.tf = tf@self.tf
+        rotation_6d = np.kron(np.eye(2,dtype=int), tf.R)
+        self.twist = rotation_6d @ self.twist
+        self.force_torque =rotation_6d @ self.force_torque
 
     @property
     def ee_link(self) -> str:
@@ -144,7 +237,7 @@ class EEState():
 
     @twist.setter
     def twist(self, twist: np.ndarray):
-        assert twist.shape[0] == 6,'Invaliid input vector size, expected {:d}, but given {:d}'.format(6, twist.shape[0])
+        assert twist.shape[0] == 6,'Invalid input vector size, expected {:d}, but given {:d}'.format(6, twist.shape[0])
         self.__twist = twist
     
     @force_torque.setter
@@ -196,14 +289,13 @@ class Robot(ABC):
     def __init__(self, urdf_filename: str, base_transform: SE3 = SE3()):
         self._urdf_filename = urdf_filename
         self._base_transform = base_transform
-        self._joint_state = JointState(6)
-        self._force_sensor_link = None
+        self._joint_state: JointState = None
         self.__controllers_types ={
             RobotControllerType.JOINT_POSITIONS.value: self._send_jointcontrol_position,
             RobotControllerType.JOINT_VELOCITIES.value: self._send_jointcontrol_velocity,
             RobotControllerType.JOINT_TORQUES.value: self._send_jointcontrol_torque,
-            RobotControllerType.TF.value: self._send_cartcontrol_position,
-            RobotControllerType.TWIST.value: self._send_cartcontrol_velocity
+            RobotControllerType.TF.value: self._send_eecontrol_position,
+            RobotControllerType.TWIST.value: self._send_eecontrol_velocity
         }
 
     @abstractmethod
@@ -215,15 +307,15 @@ class Robot(ABC):
         pass
 
     @abstractmethod
-    def _send_cartcontrol_position(self, tf: SE3()) -> bool:
-        pass
-
-    @abstractmethod
-    def _send_cartcontrol_velocity(self, velocity: np.ndarray) -> bool:
-        pass
-
-    @abstractmethod
     def _send_jointcontrol_torque(self, torque: np.ndarray) -> bool:
+        pass
+
+    @abstractmethod
+    def _send_eecontrol_position(self, tf: SE3()) -> bool:
+        pass
+
+    @abstractmethod
+    def _send_eecontrol_velocity(self, velocity: np.ndarray) -> bool:
         pass
 
     @abstractmethod
@@ -231,16 +323,13 @@ class Robot(ABC):
         pass
 
     @abstractmethod
-    def _update_cartesian_state(self, tool_state: EEState):
+    def _update_ee_state(self, tool_state: EEState):
         pass
 
     @abstractmethod
     def jacobian(self, joint_pose: np.ndarray, ee_link: str):
         pass
 
-    @abstractmethod
-    def apply_force_sensor(self, link: str):
-        pass
 
     @abstractmethod
     def reset(self):
@@ -248,6 +337,11 @@ class Robot(ABC):
 
     @abstractmethod
     def reset_joint_state(self, jstate:JointState):
+        pass
+
+    @property
+    @abstractmethod
+    def joint_limits(self) -> JointLimits:
         pass
     
     @property
@@ -262,7 +356,7 @@ class Robot(ABC):
     def ee_state(self, ee_link: str, ref_frame: str = 'world') -> EEState:
         tool_state = EEState(ee_link, ref_frame)
         self._update_joint_state(self._joint_state)
-        self._update_cartesian_state(tool_state)
+        self._update_ee_state(tool_state)
         return tool_state
 
     def set_control(self, target_motion: Motion, type_ctrl: RobotControllerType) -> bool:
@@ -278,9 +372,3 @@ class Robot(ABC):
         if not target is None:
             return self.__controllers_types[type_ctrl.value](target)
         return False
-
-
-def eestate_transform(eestate: EEState, tf: SE3):
-    eestate.tf = tf@eestate.tf
-    eestate.twist = np.kron(np.eye(2,dtype=int), tf.R) @ eestate.twist
-    eestate.force_torque = np.kron(np.eye(2,dtype=int), tf.R) @ eestate.force_torque

tests/test_pybullet_add_tool.py
@@ -11,7 +11,7 @@ from itmobotics_sim.pybullet_env.pybullet_robot import PyBulletRobot
 import unittest
 from spatialmath import SE3
 from spatialmath import base as sb
-from itmobotics_sim.utils.controllers import CartPositionToCartVelocityController, CartVelocityToJointVelocityController, JointTorquesController
+from itmobotics_sim.utils.controllers import EEPositionToEEVelocityController, EEVelocityToJointVelocityController, JointTorquesController
 
 
 target_ee_state = EEState.from_tf( SE3(0.3, -0.5, 1.0) @ SE3.Rx(np.pi), 'ee_tool')
@@ -36,10 +36,9 @@ class testPyBulletRobot(unittest.TestCase):
 
         self.__sim.add_robot(self.__robot, 'robot1')
 
-        self.__controller_speed = CartVelocityToJointVelocityController(self.__robot)
-        self.__controller_pose = CartPositionToCartVelocityController(self.__robot)
+        self.__controller_speed = EEVelocityToJointVelocityController(self.__robot)
+        self.__controller_pose = EEPositionToEEVelocityController(self.__robot)
         self.__controller_pose.connect_controller(self.__controller_speed)
-
 
         self.__sim.add_object('peg_round2', 'urdf/peg_round.urdf', base_transform = SE3(0.1, 0.0, 0.675), fixed = True, save = True)
         

tests/test_pybullet_reset.py
@@ -9,7 +9,7 @@ from itmobotics_sim.pybullet_env.pybullet_robot import PyBulletRobot
 import unittest
 from spatialmath import SE3
 from spatialmath import base as sb
-from itmobotics_sim.utils.controllers import CartPositionToCartVelocityController, CartVelocityToJointVelocityController, JointTorquesController
+from itmobotics_sim.utils.controllers import EEPositionToEEVelocityController, EEVelocityToJointVelocityController, JointTorquesController
 
 
 target_ee_state = EEState.from_tf( SE3(0.3, -0.5, 1.2) @ SE3.Rx(np.pi) , 'ee_tool')
@@ -38,15 +38,12 @@ class testPyBulletRobot(unittest.TestCase):
         self.__sim.add_robot(self.__robot, 'robot1')
         self.__sim.add_robot(self.__robot2, 'robot2')
 
-        self.__robot.apply_force_sensor('ee_tool')
-        self.__robot2.apply_force_sensor('ee_tool')
-
-        self.__controller_speed = CartVelocityToJointVelocityController(self.__robot)
-        self.__controller_pose = CartPositionToCartVelocityController(self.__robot)
+        self.__controller_speed = EEVelocityToJointVelocityController(self.__robot)
+        self.__controller_pose = EEPositionToEEVelocityController(self.__robot)
         self.__controller_pose.connect_controller(self.__controller_speed)
 
-        self.__controller_speed2 = CartVelocityToJointVelocityController(self.__robot2)
-        self.__controller_pose2 = CartPositionToCartVelocityController(self.__robot2)
+        self.__controller_speed2 = EEVelocityToJointVelocityController(self.__robot2)
+        self.__controller_pose2 = EEPositionToEEVelocityController(self.__robot2)
         self.__controller_pose2.connect_controller(self.__controller_speed2)
 
         self.__sim.add_object('hole_round2', 'urdf/hole_round.urdf', base_transform = SE3(0.1, 0.0, 0.675), fixed = True, save = True, scale_size = 1.1)

tests/test_pybullet_sim.py
@@ -9,7 +9,7 @@ from itmobotics_sim.pybullet_env.pybullet_robot import PyBulletRobot
 import unittest
 from spatialmath import SE3
 from spatialmath import base as sb
-from itmobotics_sim.utils.controllers import CartPositionToCartVelocityController, CartVelocityToJointVelocityController, JointTorquesController
+from itmobotics_sim.utils.controllers import EEPositionToEEVelocityController, EEVelocityToJointVelocityController, JointTorquesController
 
 
 target_tf = SE3(0.3, 0.0, 1.0) @ SE3.Rx(np.pi)
@@ -22,24 +22,39 @@ target_ee_state.twist = np.array([0,0,0.01,0,0,0])
 target_joint_state = JointState.from_position(np.array([0.0, -np.pi/2, 0.0, -np.pi/2, 0.0, 0.0]))
 target_joint_state = JointState.from_torque(np.zeros(6))
 
-target_motion = Motion.from_states(target_joint_state,target_ee_state)
+target_motion = Motion.from_states(target_joint_state, target_ee_state)
 target_motion2 = copy.deepcopy(target_motion)
 
 class testPyBulletRobot(unittest.TestCase):
     def setUp(self):
-        self.__sim = PyBulletWorld('plane.urdf',gui_mode = GUI_MODE.SIMPLE_GUI, time_step = 0.01, time_scale=10)
+        self.__sim = PyBulletWorld('plane.urdf',gui_mode = GUI_MODE.SIMPLE_GUI, time_step = 0.01, time_scale=5)
         self.__sim.add_object('table', 'urdf/table.urdf')
         self.__robot = PyBulletRobot('urdf/ur5e_pybullet.urdf', SE3(0,0,0.625))
         self.__sim.add_robot(self.__robot, 'robot1')
-        self.__robot.apply_force_sensor('ee_tool')
-        # self.__robot.set_jointcontrol(init_joint_state,'joint_positions')
-        self.__controller_speed = CartVelocityToJointVelocityController(self.__robot)
-        self.__controller_pose = CartPositionToCartVelocityController(self.__robot)
+        self.__controller_speed = EEVelocityToJointVelocityController(self.__robot)
+        self.__controller_pose = EEPositionToEEVelocityController(self.__robot)
         self.__controller_pose.connect_controller(self.__controller_speed)
         self.__controller_torque = JointTorquesController(self.__robot)
 
+        self.assertIsNotNone(self.__robot.joint_limits)
+        print(self.__robot.joint_limits)
+
+    def test_clip_joint_state(self):
+        js = self.__robot.joint_state
+        self.assertIsNotNone(js)
+        self.__robot.joint_limits.clip_joint_state(js)
+        self.assertTrue(np.all(js.joint_positions > self.__robot.joint_limits.limit_positions[0]))
+        self.assertTrue(np.all(js.joint_positions < self.__robot.joint_limits.limit_positions[1]))
+
+        self.assertTrue(np.all(js.joint_velocities > self.__robot.joint_limits.limit_velocities[0]))
+        self.assertTrue(np.all(js.joint_velocities < self.__robot.joint_limits.limit_velocities[1]))
+
+        self.assertTrue(np.all(js.joint_torques > self.__robot.joint_limits.limit_torques[0]))
+        self.assertTrue(np.all(js.joint_torques < self.__robot.joint_limits.limit_torques[1]))
+
     def test_sim(self):
-        self.__robot.ee_state('ee_tool')
+        self.assertIsNotNone(self.__robot.ee_state('ee_tool'))        
+        
         while self.__sim.sim_time<10.0:
             self.__sim.sim_step()
             # print(self.__robot.joint_state)