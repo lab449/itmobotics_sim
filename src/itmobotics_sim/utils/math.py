f4553baf4635f3ad5e783a892cf9e76f8fcb0b1d
Vlad1slav <vvlad1slavv@yandex.ru>
2023-05-03 12:11:33 +0300

    Add first docstring

--- src/itmobotics_sim/utils/math.py
+++ src/itmobotics_sim/utils/math.py
@@ -1,14 +1,35 @@
 from spatialmath import SE3, SO3, Twist3
 import numpy as np
+import torch as th
+import random
 
 def SE32vec(tf: SE3) -> np.ndarray:
-    result = SE3(SO3(tf)).twist().A 
+    """ SE32vec
+
+    description of function.
+
+    Args:
+        tf (SE3): _description_
+
+    Returns:
+        np.ndarray: _description_
+    """
+    result = SE3(SO3(tf)).twist().A
     result[:3] = tf.t
     return result
 
 def vec2SE3(vec: np.ndarray) -> SE3:
+    """ vec2SE3
+
+    description of function.
+
+    Args:
+        vec (np.ndarray): _description_
+
+    Returns:
+        SE3: _description_
+    """
     only_rot_vec = np.copy(vec)
     only_rot_vec[:3] = 0.0
     result = SE3(vec[:3]) @ Twist3(only_rot_vec).SE3()
     return result
-
